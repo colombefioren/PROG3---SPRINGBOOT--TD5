2f78248582895a6cd24fef91d9d77768f26cc555
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-26 18:47:14 +0300

    chore: setup project

---
+++ src/main/java/org/td5/Prog3SpringbootTd5Application.java
@@ -0,0 +1,13 @@
+package org.td5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3SpringbootTd5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog3SpringbootTd5Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/td5/Prog3SpringbootTd5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.td5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3SpringbootTd5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
