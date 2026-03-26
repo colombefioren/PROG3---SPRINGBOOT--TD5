257445d291694988390e0c21cc103f8e569d484e
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-26 17:07:18 +0300

    feat: add ingredient repository interface

--- src/main/java/org/td5/repository/IngredientRepository.java
+++ src/main/java/org/td5/repository/IngredientRepository.java
@@ -1,11 +1,15 @@
 package org.td5.repository;
 
 import org.td5.entity.Ingredient;
+import org.td5.entity.IngredientStock;
+import org.td5.entity.enums.UnitType;
 
+import java.time.Instant;
 import java.util.List;
 
 public interface IngredientRepository {
     List<Ingredient> getAllIngredients();
     Ingredient getIngredientById(Integer id);
+    IngredientStock getIngredientStockById(Integer id, Instant at, UnitType unit);
 
 }
