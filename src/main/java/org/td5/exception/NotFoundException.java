c4ea6f22c8a3f0fe88f8c05fbd0e5481ae8b37e6
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-26 17:43:54 +0300

    refactor: make custom not found exception extends from exception instead of unchecked exc

--- src/main/java/org/td5/exception/NotFoundException.java
+++ src/main/java/org/td5/exception/NotFoundException.java
@@ -1,6 +1,6 @@
 package org.td5.exception;
 
-public class NotFoundException extends RuntimeException {
+public class NotFoundException extends Exception {
   public NotFoundException(String message) {
     super(message);
   }
