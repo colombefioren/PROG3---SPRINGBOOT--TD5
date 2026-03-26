5a9e9e77830ed6611da5bea2a34e469a75aef629
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-26 17:42:10 +0300

    feat: create get ingredient/{id} in ingredient controller

--- src/main/java/org/td5/controller/IngredientController.java
+++ src/main/java/org/td5/controller/IngredientController.java
@@ -1,26 +1,33 @@
 package org.td5.controller;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.td5.entity.Ingredient;
 import org.td5.service.IngredientService;
 
-import java.util.List;
-
 @RestController
 @AllArgsConstructor
 public class IngredientController {
-    private final IngredientService service;
+  private final IngredientService service;
 
-    @GetMapping("/ingredients")
-    public ResponseEntity<List<Ingredient>> getAllIngredients(){
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(service.getAllIngredients());
+  @GetMapping("/ingredients")
+  public ResponseEntity<List<Ingredient>> getAllIngredients() {
+    return ResponseEntity.status(HttpStatus.OK).body(service.getAllIngredients());
+  }
+
+  @GetMapping("/ingredients/{id}")
+  public ResponseEntity<?> getIngredientById(@PathVariable Integer id) {
+    try {
+      return ResponseEntity.status(HttpStatus.OK).body(service.getIngredientById(id));
+    } catch (NotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
-
-
+  }
 }
--- src/main/java/org/td5/service/IngredientService.java
+++ src/main/java/org/td5/service/IngredientService.java
@@ -1,18 +1,21 @@
 package org.td5.service;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.td5.entity.Ingredient;
 import org.td5.repository.IngredientRepository;
 
-import java.util.List;
-
 @Service
 @AllArgsConstructor
 public class IngredientService {
-    private final IngredientRepository repository;
+  private final IngredientRepository repository;
 
-    public List<Ingredient> getAllIngredients(){
-        return repository.getAllIngredients();
-    }
+  public List<Ingredient> getAllIngredients() {
+    return repository.getAllIngredients();
+  }
+
+  public Ingredient getIngredientById(Integer id) {
+    return repository.getIngredientById(id);
+  }
 }
