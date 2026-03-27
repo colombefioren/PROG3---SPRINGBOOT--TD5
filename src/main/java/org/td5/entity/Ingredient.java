a2fe49c3c506897a33f13c5655134c12172d861e
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-27 09:38:14 +0300

    feat: implement get stock value method in ingredient entity

--- src/main/java/org/td5/entity/Ingredient.java
+++ src/main/java/org/td5/entity/Ingredient.java
@@ -1,23 +1,42 @@
 package org.td5.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.time.Instant;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.td5.entity.enums.CategoryEnum;
-
-import java.util.List;
+import org.td5.entity.enums.MovementTypeEnum;
+import org.td5.entity.enums.UnitType;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Ingredient {
-    private Integer id;
-    private String name;
-    private CategoryEnum category;
-    private Double price;
-    @JsonIgnore
-    private List<StockMovement> stockMovementList;
+  private Integer id;
+  private String name;
+  private CategoryEnum category;
+  private Double price;
+  @JsonIgnore private List<StockMovement> stockMovementList;
+
+  public StockValue getStockValue(Instant instant, UnitType unit) {
+    if (stockMovementList == null || stockMovementList.isEmpty()) {
+      return new StockValue(0.0, UnitType.KG);
+    }
+    double total = 0.0;
+    for (StockMovement movement : stockMovementList) {
+
+      if (!movement.getCreationDatetime().isAfter(instant)) {
+        if (movement.getType().equals(MovementTypeEnum.IN)) {
+          total += movement.getValue().getQuantity();
+        } else if (movement.getType().equals(MovementTypeEnum.OUT)) {
+          total -= movement.getValue().getQuantity();
+        }
+      }
+    }
+    return new StockValue(total, UnitType.KG);
+  }
 }
