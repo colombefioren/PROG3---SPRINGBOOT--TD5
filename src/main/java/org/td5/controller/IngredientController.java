7e00f452144f1857005033ed8b1e6c226bac8b24
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-27 09:54:38 +0300

    feat: create get ingredient/:id/stock endoint with at and unit request params

--- src/main/java/org/td5/controller/IngredientController.java
+++ src/main/java/org/td5/controller/IngredientController.java
@@ -1,30 +1,46 @@
 package org.td5.controller;
 
+import java.time.Instant;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.td5.entity.Ingredient;
+import org.td5.entity.enums.UnitType;
 import org.td5.exception.NotFoundException;
 import org.td5.service.IngredientService;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/ingredients")
 public class IngredientController {
   private final IngredientService service;
 
-  @GetMapping("/ingredients")
+  @GetMapping
   public ResponseEntity<List<Ingredient>> getAllIngredients() {
     return ResponseEntity.status(HttpStatus.OK).body(service.getAllIngredients());
   }
 
-  @GetMapping("/ingredients/{id}")
+  @GetMapping("/{id}")
   public ResponseEntity<?> getIngredientById(@PathVariable Integer id) {
     try {
       return ResponseEntity.status(HttpStatus.OK).body(service.getIngredientById(id));
+    } catch (NotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+  }
+
+  @GetMapping("/{id}/stock")
+  public ResponseEntity<?> getIngredientStockValue(@PathVariable Integer id, @RequestParam(required = false) Instant at, @RequestParam(required = false) UnitType unit) {
+    if(at == null || unit == null){
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Either mandatory query parameter `at` or `unit` is not provided");
+    }
+    try {
+      return ResponseEntity.status(HttpStatus.OK).body(service.getIngredientStockValue(id, at, unit));
     } catch (NotFoundException e) {
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     } catch (Exception e) {
