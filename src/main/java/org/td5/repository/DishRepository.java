0c41fe6429bfe32a6e3c27cc911eeb3f1073dc1e
colombefioren <colomberakotonjanahary@gmail.com>
2026-03-27 10:18:46 +0300

    feat: implement find all dishes in dish repository implementation

--- src/main/java/org/td5/repository/DishRepository.java
+++ src/main/java/org/td5/repository/DishRepository.java
@@ -1,6 +1,7 @@
 package org.td5.repository;
 
 import org.td5.entity.Dish;
+import org.td5.entity.DishIngredient;
 import org.td5.entity.Ingredient;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface DishRepository {
     List<Dish> findAll();
     Dish updateIngredientsInDish(Integer dishId, List<Ingredient> ingredientList);
+    List<DishIngredient> findDishIngredientsByDishId(Integer dishId);
 }
--- src/main/java/org/td5/repository/implementation/DishRepositoryImpl.java
+++ src/main/java/org/td5/repository/implementation/DishRepositoryImpl.java
@@ -4,9 +4,15 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.td5.configuration.DataSource;
 import org.td5.entity.Dish;
+import org.td5.entity.DishIngredient;
 import org.td5.entity.Ingredient;
 import org.td5.repository.DishRepository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -16,11 +22,43 @@ public class DishRepositoryImpl implements DishRepository {
 
     @Override
     public List<Dish> findAll() {
-        return List.of();
+        String sql =
+                """
+                            select d.id as d_id, d.name as d_name, d.dish_type, d.selling_price as d_price from dish d order by d.id
+                        """;
+
+        List<Dish> dishes = new ArrayList<>();
+
+        try(Connection con = dataSource.getDBConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery()
+            ){
+
+            while (rs.next()) {
+                dishes.add(mapResultSetToDish(rs));
+            }
+            return dishes;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Dish updateIngredientsInDish(Integer dishId, List<Ingredient> ingredientList) {
         return null;
+    }
+
+    @Override
+    public List<DishIngredient> findDishIngredientsByDishId(Integer dishId) {
+        return List.of();
+    }
+
+    private Dish mapResultSetToDish(ResultSet rs) throws SQLException {
+        return Dish.builder()
+                .id(rs.getInt("d_id"))
+                .name(rs.getString("d_name"))
+                .price(rs.getDouble("d_price"))
+                .build();
     }
 }
